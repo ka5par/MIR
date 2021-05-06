bf3b4825f7ad3f071de19e7f79a0dc9a02f497f6
ka5par <karlkasparhaavel@gmail.com>
2021-05-07 00:27:35 +0300

    refactoring, change venv to 3.7, tensorflow 1.15

---
+++ main.py
@@ -0,0 +1,12 @@
+import myller.extractor as me
+import pop_music_highlighter.extractor as pmhe
+
+if __name__ == '__main__':
+    # fs = ['../data/Pink Floyd - The Great Gig in The Sky.wav']  # list
+    fs = ["../data/Pink Floyd - The Great Gig in The Sky.wav", "../data/FMP_C4_Audio_Beatles_YouCantDoThat.wav"]
+
+    # myller
+    me.extract(fs, length=10, save_SSM=True, save_thumbnail=True, save_wav=True, save_SP=True)
+
+    # pop_music_highlighter
+    pmhe.extract(fs, length=10, save_score=True, save_thumbnail=True, save_wav=True)
